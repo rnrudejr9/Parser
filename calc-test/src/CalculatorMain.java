fd3d2002126aac701d1585d19e95916bfd7d1aac
Koogling <rnrudejr1234@naver.com>
2022-10-05 10:19:57 +0900

    commit test

---
+++ calc-test/src/CalculatorMain.java
@@ -0,0 +1,11 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+
+        System.out.println(calc.plus(2,3));
+        System.out.println(calc.minus(2,3));
+        System.out.println(calc.muliple(2,3));
+        System.out.println(calc.divide(2,3));
+    }
+}
