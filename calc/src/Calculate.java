ed819421c1f7899eb0d9c6b1feb768bb8b64c213
Koogling <rnrudejr1234@naver.com>
2022-10-05 10:35:23 +0900

    Revert "calc test"
    This reverts commit bd558fa7fdaac9085ba16ef5badeee83747be10c.

--- calc/src/Calculate.java
+++ calc/src/Calculate.java
@@ -1,14 +1,2 @@
-public class Calculate {
-    public void plus(int a,int b){
-        System.out.println(a+b);
-    }
-    public void minus(int a,int b){
-        System.out.println(a-b);
-    }
-    public void multiple(int a,int b){
-        System.out.println(a*b);
-    }
-    public void divide(double a,double b){
-        System.out.println(a/b);
-    }
+package PACKAGE_NAME;public class Calculate {
 }
--- calc/src/CalculateMain.java
+++ calc/src/CalculateMain.java
@@ -1,11 +1,2 @@
-public class CalculateMain {
-    public static void main(String[] args) {
-        Calculate c = new Calculate();
-        int a = 10;
-        int b = 20;
-        c.plus(a,b);
-        c.minus(a,b);
-        c.multiple(a,b);
-        c.divide(a,b);
-    }
+package PACKAGE_NAME;public class CalculateMain {
 }
