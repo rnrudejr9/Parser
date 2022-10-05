a289aa31fb45b68a0386691477cb5f6f31ea6cd6
Koogling <rnrudejr1234@naver.com>
2022-10-05 10:36:37 +0900

    update

--- CodeLionBOJ/src/p_1021_회전하는큐.java
+++
@@ -1,5 +0,0 @@
-public class p_1021_회전하는큐 {
-    public static void main(String[] args) {
-        
-    }
-}
--- CodeLionBOJ/src/p_2108_통계학.java
+++
@@ -1,6 +0,0 @@
-import java.io.BufferedReader;
-
-public class p_2108_통계학 {
-    public static void main(String[] args) {
-    }
-}
