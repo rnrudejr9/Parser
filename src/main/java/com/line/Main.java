ca6f5f4a71ae67582e02f05a361090885e7ef143
Koogling <rnrudejr1234@naver.com>
2022-10-18 11:32:44 +0900

    UserDAO add, delete, select commit

--- src/main/java/com/line/Main.java
+++ src/main/java/com/line/Main.java
@@ -4,6 +4,7 @@ package com.line;
 import com.line.database.DBLoader;
 import com.line.database.UserDAO;
 import com.line.domain.Hospital;
+import com.line.domain.User;
 import com.line.parser.HospitalParser;
 
 import java.io.IOException;
@@ -16,34 +17,21 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
-        LineReader<Hospital> lr = new LineReader<>(new HospitalParser());
-        String filename = "C:\\Users\\KOO\\git\\CodeLion-Git\\서울시 병의원 위치 정보.csv";
-        List<Hospital> hospitals = lr.readLines(filename);
-        //lr.readLines 로 csv 파일 내 정보를 hospitals 객체에 정보들을 저장
-
-        SQLWriter<Hospital> sw = new SQLWriter<>(new HospitalParser());
-        sw.sqlWrite("hospital.sql",hospitals);
+//        LineReader<Hospital> lr = new LineReader<>(new HospitalParser());
+//        String filename = "C:\\Users\\KOO\\git\\CodeLion-Git\\서울시 병의원 위치 정보.csv";
+//        List<Hospital> hospitals = lr.readLines(filename);
+//        //lr.readLines 로 csv 파일 내 정보를 hospitals 객체에 정보들을 저장
+//
+//        SQLWriter<Hospital> sw = new SQLWriter<>(new HospitalParser());
+//        sw.sqlWrite("hospital.sql",hospitals);
         //sw write 메소드로 hospital list 에 있는 정보들을 hospital.sql 파일에 쓰기
 
-        String url = "jdbc:mysql://localhost/likelion-db";
-        String user = "root";
-        String pw = "12345";
-        DBLoader db = new DBLoader(url,user,pw);
-        //database 연동과정
 
-//        db.loadDatabase(hospitals);
-        //sql파일을 database에 insert 하는 과정
-
-
-        db.readDatabase();
-        //database에 있는 값들을 불러오는 과정
-
-
-//        List<UserDAO> dao = new ArrayList<>();
-//        UserDAO userdao = new UserDAO(1,"hello","world");
-//        //userdao.add(userdao);
-//        userdao.search();
-        //userdao 연습문제
+        UserDAO dao = new UserDAO();
+        try {
+            dao.delete(new User());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/line/database/UserDAO.java
+++ src/main/java/com/line/database/UserDAO.java
@@ -1,6 +1,7 @@
 package com.line.database;
 
 import com.line.domain.Hospital;
+import com.line.domain.User;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -8,87 +9,61 @@ import java.util.Arrays;
 import java.util.List;
 
 public class UserDAO {
-    private int id;
-    private String password;
-    private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public UserDAO(int id, String password, String name) {
-        this.id = id;
-        this.password = password;
-        this.name = name;
-    }
-
-    public void add(UserDAO dao){
-        try {
-            String url = "jdbc:mysql://localhost/likelion-db";
-            String user = "root";
-            String pw = "12345";
-            Connection c = DriverManager.getConnection(url, user, pw);
-
-                PreparedStatement ps = c.prepareStatement(
-                        "insert into usertable(id,password,name) values(?,?,?)");
-                ps.setInt(1, dao.getId());
-                ps.setString(2, dao.getPassword());
-                ps.setString(3, dao.getName());
-                ps.executeUpdate();
-                ps.setInt(1,1);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
     public UserDAO(){
 
     }
-    public void search(){
-        try {
-            String url = "jdbc:mysql://localhost/likelion-db";
-            String user = "root";
-            String pw = "12345";
-            Connection c = DriverManager.getConnection(url, user, pw);
+    private Connection getConnection() throws SQLException {
+
+        String url = "jdbc:mysql://localhost/likelion-db";
+        String user = "root";
+        String pw = "12345";
+
+        Connection c = DriverManager.getConnection(url,user,pw);
+        return c;
+    }
+
+    public void add(User user) throws SQLException {
+        Connection c = getConnection();
+        String sql = "insert into usertable(id,name,password) values (?,?,?)";
+        PreparedStatement ps = c.prepareStatement(sql);
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+    public void delete(User user) throws SQLException {
+        Connection c = getConnection();
+        String sql = "delete from usertable";
+        PreparedStatement ps = c.prepareStatement(sql);
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public void search() throws SQLException {
+            Connection c = getConnection();
             String sql = "select * from usertable";
             PreparedStatement ps = c.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            ArrayList<UserDAO> list = new ArrayList<>();
+            List<User> list = new ArrayList<>();
             while (rs.next()){
-                UserDAO dao = new UserDAO();
-                dao.setId(rs.getInt("id"));
-                dao.setPassword(rs.getString("password"));
-                dao.setName(rs.getString("name"));
-                list.add(dao);
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setPassword(rs.getString("name"));
+                user.setName(rs.getString("password"));
+               list.add(user);
             }
-            for(UserDAO temp : list){
+            for(User temp : list){
                 System.out.println(temp.getId());
-                System.out.println(temp.getPassword());
                 System.out.println(temp.getName());
+                System.out.println(temp.getPassword());
             }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+            ps.close();
+            c.close();
     }
 }
