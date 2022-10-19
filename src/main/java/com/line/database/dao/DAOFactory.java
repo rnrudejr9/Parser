ee581c14c88218af6c6e56b717e99f436d801e9c
Koogling <rnrudejr1234@naver.com>
2022-10-19 14:20:23 +0900

    spring 적용

--- src/main/java/com/line/database/dao/DAOFactory.java
+++ src/main/java/com/line/database/dao/DAOFactory.java
@@ -2,11 +2,18 @@ package com.line.database.dao;
 
 import com.line.database.connection.ConnectionMaker;
 import com.line.database.connection.LDAOConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DAOFactory {
+
+    @Bean
     public UserDAO userDao(){
         return new UserDAO(connectionMaker());
     }
+
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new LDAOConnection();
     }
--- src/test/java/com/line/database/dao/UserDAOTest.java
+++ src/test/java/com/line/database/dao/UserDAOTest.java
@@ -4,21 +4,30 @@ import com.line.database.connection.ConnectionMaker;
 import com.line.database.connection.LDAOConnection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DAOFactory.class)
 class UserDAOTest {
+    @Autowired
+    ApplicationContext context;
+
     @Test
     public void deleteAll() throws SQLException {
+        UserDAO userdao = context.getBean("userDao",UserDAO.class);
+
         UserDAO userDao = new DAOFactory().userDao();
         userDao.getCount();
     }
 
-    @Test
-    public void add(){
-
-    }
 
 }
